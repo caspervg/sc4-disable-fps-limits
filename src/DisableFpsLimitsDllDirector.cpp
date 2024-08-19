570f199bfc6e49f649bec7138c101d75cfc5230c
Casper Van Gheluwe <caspervg@gmail.com>
2024-08-19 11:44:43 +0200

    Update release version

--- src/DisableFpsLimitsDllDirector.cpp
+++ src/DisableFpsLimitsDllDirector.cpp
@@ -3,7 +3,7 @@
 // This file is part of sc4-disable-fps-limits, a DLL Plugin
 // for SimCity 4 that disables the default FPS limitation while the simulation
 // is running in Turtle (slow simulation, 30 FPS), Rhino (medium
-// simulation, 20 FPS) and Cheetah (fast simulation , 15 FPS) speed
+// simulation, 20 FPS) and Cheetah (fast simulation , 15 FPS) speed.
 // 
 // Copyright (c) 2024 Casper Van Gheluwe
 // Heavily inspired by sc4-disable-network-construction-sounds by Nicolas Hayes
--- src/version.h
+++ src/version.h
@@ -1,10 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// This file is part of sc4-disable-network-construction-sounds, a DLL Plugin
-// for SimCity 4 that disables the sounds that are played during the network
-// construction animation.
-//
-// Copyright (c) 2024 Nicholas Hayes
+// This file is part of sc4-disable-fps-limits, a DLL Plugin
+// for SimCity 4 that disables the default FPS limitation while the simulation
+// is running in Turtle (slow simulation, 30 FPS), Rhino (medium
+// simulation, 20 FPS) and Cheetah (fast simulation , 15 FPS) speed.
+// 
+// Copyright (c) 2024 Casper Van Gheluwe
+// Heavily inspired by sc4-disable-network-construction-sounds by Nicolas Hayes
 //
 // This file is licensed under terms of the MIT License.
 // See LICENSE.txt for more information.
@@ -13,6 +15,6 @@
 
 #pragma once
 
-#define PLUGIN_VERSION_STR	    "1.0.0"
-#define RESOURCE_VERSION         1,0,0,0
-#define RESOURCE_VERSION_STR    "1.0.0.0"
+#define PLUGIN_VERSION_STR	    "0.1.1"
+#define RESOURCE_VERSION         0,1,1,0
+#define RESOURCE_VERSION_STR    "0.1.1.0"
